fbef9f480eebe71aa866edc228a3cd34eb210dc5
Tali Avni <tali@aristo7.com>
2019-10-04 08:57:55 -0700

    some css changes and score counter

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+/* eslint-disable no-console */
 import getRandomThrow from './get-random-throw.js';
 import checkResult from './check-result.js';
 
+let numberOfWins = 0;
+let numberOfLosses = 0;
+let numberOfDraws = 0;
+
 const myButton = document.getElementById('button');
 
+const updateSpans = () => {
+    winSpan.textContent = numberOfWins;
+    loseSpan.textContent = numberOfLosses;
+    drawSpan.textContent = numberOfDraws;
+};
+
+const winSpan = document.getElementById('win');
+const loseSpan = document.getElementById('lose');
+const drawSpan = document.getElementById('draw');
 
 myButton.onclick = () => { 
     const playerChoice = document.querySelector('input:checked').value;
@@ -10,43 +24,44 @@ myButton.onclick = () => {
     const randomNumber = Math.floor(Math.random() * 3 + 1);
 
     let computerChoice = getRandomThrow(randomNumber);
+    // if (computerChoice === 'rock') {
+        // .removeClass('hidden');
+    // } if (computerChoice === 'paper') {
+        // .removeClass('hidden2');
+    // } if (computerChoice === 'scissors') {
+        // .removeClass('hidden3');
+    // } 
+
 
     let result = checkResult(playerChoice, computerChoice);
 
     if (result === 'win') {
+        numberOfWins ++;
         console.log('win');
-        console.log(computerChoice)
-        console.log(playerChoice)
-    } else if (result ==='lose') {
+        console.log(computerChoice);
+        console.log(playerChoice);
+    } else if (result === 'lose') {
+        numberOfLosses ++;
         console.log('lose');
-        console.log(computerChoice)
-        console.log(playerChoice)
+        console.log(computerChoice);
+        console.log(playerChoice);
     } else {
+        numberOfDraws ++;
         console.log('draw');
-        console.log(computerChoice)
-        console.log(playerChoice)
+        console.log(computerChoice);
+        console.log(playerChoice);
     } 
-}
+    updateSpans();
+};
 
 
 
-   
-
-// const winSpan = document.getElementById('win');
-// const loseSpan = document.getElementById('lose');
-// const drawSpan = document.getElementById('draw');
 //const summarySpan = document.getElementById('summary');
 //const resetButton = document.getElementById('reset-button');
 
-// let numberOfWins = 0;
-// let numberOfLosses = 0;
-// let numberOfDraws = 0;
 
-//const updateSpans = () => {
-//    winSpan.textContent = numberOfWins;
-//    loseSpan.textContent = numberOfLosses;
-    // drawSpan.textContent = numberOfDraws;
-// }
+
+
 
 
 myButton.addEventListener('click', checkResult);
--- get-random-throw.js
+++ get-random-throw.js
@@ -2,11 +2,11 @@
 
 const getRandomThrow = (number) => {
     if (number === 1) {
-    return 'rock';
-} else if (number === 2) {
-    return 'paper';
-} else if (number === 3) {
-    return 'scissors';
-}}
+        return 'rock';
+    } else if (number === 2) {
+        return 'paper';
+    } else if (number === 3) {
+        return 'scissors';
+    }};
 
-export default getRandomThrow
+export default getRandomThrow;
